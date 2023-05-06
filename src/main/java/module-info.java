796ed1fc7d13e35a871a059a8e74d899ac8f6332
zazed <zazed@localhost.localdomain>
2023-05-06 15:17:06 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ui.sentimentalyst {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens ui.sentimentalyst to javafx.fxml;
+    exports ui.sentimentalyst;
+}
