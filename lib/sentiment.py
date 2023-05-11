5f264ccd34294bf6d18f16b09caca9ceeabf40a0
zazed <zazed@localhost.localdomain>
2023-05-11 12:19:39 +0100

    added tests, added richtext, made some changes to python code

--- lib/sentiment.py
+++ lib/sentiment.py
@@ -3,8 +3,29 @@ import sys
 
 nlp = stanza.Pipeline('en', verbose=False, model_dir="./stanza")
 
-args = sys.argv
+doc = nlp(input())
 
-doc = nlp(args[1])
+sum = 0
+lasti = 0
+
 for i, sentence in enumerate(doc.sentences):
-    print("%d|%d" % (i, sentence.sentiment))
+    sent = sentence.sentiment
+    print("%d|%d" % (i, sent))
+    sum += sent
+    lasti = i + 1
+
+score = sum / lasti
+print(score)
+
+if (lasti == 1): 
+    if (score == 0): print("Triste")
+    elif (score == 1): print("Neutro")
+    elif (score == 2): print("Feliz")
+
+elif (score < 0.40): print("Muito Triste")
+elif (score < 0.90): print("Triste")
+elif (score < 1.10): print("Neutro")
+elif (score < 1.60): print("Feliz")
+elif (score < 2): print("Muito Feliz")
+
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,6 @@
 module ui.sentimentalyst {
     requires javafx.controls;
     requires javafx.fxml;
-    requires stanford.corenlp;
-
 
     opens ui.sentimentalyst to javafx.fxml;
     exports ui.sentimentalyst;
