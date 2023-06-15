e25a1a2d7a52932e79ba171593ed059328ee812a
Leonardo Santos <93401987+zazedd@users.noreply.github.com>
2023-06-15 22:32:52 +0100

    Delete languageid.py

--- lib/languageid.py
+++
@@ -1,9 +0,0 @@
-import stanza
-import sys
-
-nlp = stanza.Pipeline('multilingual', verbose=False, processors="langid", model_dir="./stanza")
-
-args = sys.argv
-
-doc = nlp(args[1])
-print(doc.lang)
